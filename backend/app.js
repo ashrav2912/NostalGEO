9e22616315b62b56ec5812c573ead703b5e4a5d1
ayaan Pathan <ayaan.path@gmail.com>
2024-01-27 17:55:05 -0500

    database structural changes

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,9 @@ const port = 3000;
 const db = new sqlite3.Database(path.join(__dirname, '/db/app.db'));
 
 app.get('/create_marker', (req, res) => {
-    db.run('INSERT INTO markers (location) VALUES (?)', [req.query.location], function(err) {
+    const lat = req.query.location.split(',')[0];
+    const long = req.query.location.split(',')[1];
+    db.run('INSERT INTO markers (_id, lat, long) VALUES (?, ?)', [lat, long], function(err) {
         if (err) {
             return console.log(err.message);
         }
@@ -19,10 +21,22 @@ app.get('/get_markers', (req, res) => {
         if (err) {
             throw err;
         }
+        console.log(JSON.stringify(rows));
         res.send(rows);
+
     });
 });
 
+
+
+app.get('/create_time_capsule', (req, res) => {
+    db.run('INSERT INTO time_capsules (_id, author_id, marker_id, date) VALUES (?, 1, ?,)', [req.query.location, req.query.date, req.query.message], function(err) {
+        if (err) {
+            return console.log(err.message);
+        }
+        res.send(`A row has been inserted with rowid ${this.lastID}`);
+    });
+});
 
 
 
