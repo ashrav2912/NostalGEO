d604e19d14ea1ccc4c8c85a79b91840c5d3151cd
ashrav2912 <akshayravikumar2912@gmail.com>
2024-01-28 01:33:15 -0500

    Added the gallery to the home page

--- frontend/home_page.py
+++ frontend/home_page.py
@@ -2,10 +2,15 @@ import folium
 import streamlit as st
 from bokeh.models import Button, CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
-
 from streamlit_folium import st_folium
 from get_curr_loc import get_curr_loc
 import requests
+from streamlit_modal import Modal
+from gallery import gallery
+
+import folium
+from jinja2 import Template
+
 # def home_page():
 #     # center on Liberty Bell, add marker
 #     m = folium.Map(location=get_curr_loc(), zoom_start=16)
@@ -19,7 +24,10 @@ import requests
 # if __name__ == '__main__':
 #     home_page()
 
-
+st.set_page_config(
+        page_title="My Page Title",
+        layout="wide"
+)
 
 
 def location():
@@ -41,7 +49,7 @@ def location():
     # st.bokeh_chart(result)
     if(result):
         latlong = [result['GET_LOCATION']['lat'], result['GET_LOCATION']['lon']]
-        print(latlong)
+        # print(latlong)
         return latlong
     else:
         return [0,0]
@@ -49,28 +57,41 @@ def location():
 def home_page():
     # center on Liberty Bell, add marker
     latlong = location()
+
     m = folium.Map(location=latlong, zoom_start=16)
     # folium.Marker(
     #     app(), popup="Your location", tooltip="Your location"
     # ).add_to(m)
 
     # call to render Folium map in Streamlit
+    
+
+    # Modify Marker template to include the onClick event
+    
     count = 0
     response = requests.get("http://localhost:3000/get_markers")
-    print("Response: ", response.text)
-    print("Type: ", type(response.text))
+    # print("Response: ", response.text)
+    # print("Type: ", type(response.text))
     json_list = eval(response.text)
-    print("Json list: ", json_list)
-    print(type(json_list) , "wrong")
-    folium.Marker(
-    latlong, popup="Your location", tooltip="Your location"
-).add_to(m) 
+    # print("Json list: ", json_list)
+    # print(type(json_list) , "wrong")
+    
+    # Current Location
+    print(folium.Marker(latlong, popup="Your location", tooltip="Your location").add_to(m)) 
+    
     for ele in json_list:
         coords = eval(ele['location'])
         coords = list(coords)
-        folium.Marker(
-        coords, popup="Location", tooltip="location").add_to(m)
+        folium.Marker(coords, popup="Location", tooltip="location").add_to(m)
+    
+    
     st_data = st_folium(m, width=725)
+    print(st_data["last_object_clicked_popup"])
+    modal = Modal(key="Demo Key",title="Pop-up")
+    if(st_data["last_object_clicked_popup"] == "Your location"):
+        gallery()
+# with modal.container():
+#     st.markdown('testtesttesttesttesttesttesttest')
 
 if __name__ == '__main__':
     home_page()
